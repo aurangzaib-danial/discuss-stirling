22e472da480e1033af615909a44f4b5110b27125
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-02 14:02:23 +0000

    implemented sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to sign_in_path
+    end
+  end
   def destroy
     session.delete(:user_id)
     redirect_to root_path
