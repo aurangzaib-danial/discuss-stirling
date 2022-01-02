6611e7c0675a750f40907a45319c18dbf94c8da2
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-02 11:14:23 +0000

    added navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root "main#index"
+
+  get "sign-up", to: "registrations#new"
+  post "sign-up", to: "registrations#create"
+
+  get "sign-in", to: "sessions#new"
+  post "sign-in", to: "sessions#create"
+
 end
