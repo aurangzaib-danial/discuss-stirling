a2965a51054ee32b1d9bd5ca2710a5228af3f20d
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-02 13:48:29 +0000

    implemented sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+  helper_method :user_signed_in?
+
+  def set_current_user
+    Current.user = User.find(session[:user_id]) if session[:user_id].present?
+  end
+
+  def user_signed_in?
+    Current.user.present?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    User.create(name: 'Sunny', email: 'a@a.com', password: '123456', private: true)
+    assert User.last.private?
+  end
 end
