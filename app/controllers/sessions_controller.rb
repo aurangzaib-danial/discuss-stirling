353b559ea47538a820ecee9a21a7305049e64642
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-03 10:30:00 +0000

    improved sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,27 @@
 class SessionsController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def create
-    @user = User.find_by_email(params[:email])
-    if @user.present? && @user.authenticate(params[:password])
+    @user = User.find_by_email(user_params[:email])
+    if @user.present? && @user.authenticate(user_params[:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      redirect_to sign_in_path
+      @user.errors.add(:sign_in, "The email or password is incorrect.")
+      render :new, status: :unprocessable_entity
     end
   end
+
   def destroy
     session.delete(:user_id)
     redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user)
   end
 end
