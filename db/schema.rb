6324156cb8f390ad4235b73cd92a10b73ac316ab
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-03 23:39:51 +0000

    added index on users email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_02_010225) do
+ActiveRecord::Schema.define(version: 2022_01_03_233848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_01_02_010225) do
     t.boolean "private", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
