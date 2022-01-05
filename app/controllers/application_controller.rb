ac7dbcd3e7949a8b1f32ee20ac0ead2b6d9fcd57
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-05 00:18:01 +0000

    redirect user to select account type if they haven't which is the case after a facebook sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
+  before_action :set_current_user, if: :user_signed_in?
+  before_action :ask_user_to_select_account_type, if: :user_signed_in?
   helper_method :user_signed_in?
 
   def set_current_user
-    Current.user = User.find(session[:user_id]) if session[:user_id].present?
+    Current.user = User.find(session[:user_id])
   end
 
   def user_signed_in?
-    Current.user.present?
+    session[:user_id].present?
+  end
+
+  def ask_user_to_select_account_type
+    redirect_to select_account_path unless Current.user.account_type_selected?
   end
 
   def login(user)
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < ApplicationController
+  before_action :ask_user_to_select_account_type, if: :user_signed_in?, except: :select_account
+  
   def new
     if !params[:account].present? || !params[:account].in?(["private", "public"])
       redirect_to select_account_path
