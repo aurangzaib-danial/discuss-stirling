f9ac7214265cea480f25396a4dcd897a104b9fe7
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-06 23:32:18 +0000

    switch from Sign out to Log out

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
 
-  delete "sign-out", to: "sessions#destroy"
+  delete "logout", to: "sessions#destroy"
 
   get "/auth/facebook/callback", to: "omniauth_callbacks#facebook"
 end
