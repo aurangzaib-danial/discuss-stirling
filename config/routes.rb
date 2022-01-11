5936d96a72f360e104de907edd4c907303e8676d
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-11 10:39:22 +0000

    generated devise registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do  
   
-  devise_for :users, path_names: { sign_in: "login", sign_out: "logout", sign_up: "signup" }
+  devise_for(
+    :users, 
+    path_names: { sign_in: "login", sign_out: "logout", sign_up: "signup" }
+    controllers: { registrations: 'users/registrations'}
   
   root "main#index"
 
--- test/controllers/sessions_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "the truth" do
-    login users(:regular)
-    get login_path
-    assert_redirected_to root_path
-  end
-end
