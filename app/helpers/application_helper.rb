9baf4c05358054fa8506d9634830f6ad1f3f7fcc
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-12 10:51:50 +0000

    answers are now being displayed on question show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     text += ' - Discuss Stirling' unless home
     content_for :title, text
   end
+
+  def created_at_in_words(object)
+    distance_of_time_in_words_to_now(object.created_at) + ' ago'
+  end
+
+  def user_info(user)
+    user.private_account? ? "Private User" : user.name
+  end
 end
