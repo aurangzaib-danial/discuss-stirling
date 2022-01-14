165a613064354826e5284d6578aa19ffe5e293af
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-14 23:00:52 +0000

    implemented validations for answer's body

--- app/javascript/controllers/new_question_form_controller.js
+++ app/javascript/controllers/new_question_form_controller.js
@@ -2,7 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="new-question-form"
 export default class extends Controller {
+  static targets = ["body", "errorMessage"]
+
+  validations(event) {
+    if (this.bodyTarget.textContent === "") {
+      this.renderError(event, "Body can't be blank")
+    } else if (this.bodyTarget.textContent.length <= 30) {
+      this.renderError(event, "Body is too short (minimum is 30 characters)")
+    }
+  }
+
+  renderError(event, message) {
+    event.preventDefault()
+    this.bodyTarget.classList.add("is-invalid")
+    this.errorMessageTarget.innerHTML = message
+  }
+
   reset() {
+    this.bodyTarget.classList.remove("is-invalid")
     this.element.reset()
   }
 }
