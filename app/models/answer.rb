f619e77eab36c2d06736b62b9bbe83cc7c2bf2a5
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-14 21:50:27 +0000

    created body validations module

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,5 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
   has_rich_text :body
+  include BodyValidations
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -5,12 +5,5 @@ class Question < ApplicationRecord
   has_many :answers
 
   validates :title, presence: true, length: { minimum: 15, maximum: 150 }
-  validates :body, presence: true
-  validate :length_of_body
-
-  def length_of_body
-    if body.to_plain_text.length <= 30
-     errors.add(:body, "is too short (minimum is 30 characters)") 
-    end
-  end
+  include BodyValidations
 end
