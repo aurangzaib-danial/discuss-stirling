d25c9c7a07dbeb491f08f8dc631f6b222f136466
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-15 21:12:00 +0000

    wrote vote? and update? methdos in Question policy

--- app/policies/question_policy.rb
+++ app/policies/question_policy.rb
@@ -1,7 +1,21 @@
 class QuestionPolicy < ApplicationPolicy
+
+  def vote?
+    true unless user == question.questioner
+  end
+
+  def update?
+    user == question.questioner
+  end
+
   class Scope < Scope
     def resolve
       scope.all
     end
+  end
+
+  private
+  def question
+    record
   end
 end
--- test/policies/question_policy_test.rb
+++ test/policies/question_policy_test.rb
@@ -1,26 +1,26 @@
 require 'test_helper'
 
 class QuestionPolicyTest < ActiveSupport::TestCase
-  # def test_scope
-  # end
-
-  # def test_show
-  # end
-
-  # def test_create
-  # end
-
-  # def test_update
-  # end
-
-  # def test_destroy
-  # end
-
-  setup do
-    @policy = Pundit.policy! users(:one), questions(:one)
-  end
-
   test "cannot vote question if the user themselves created the question" do
-    
+    policy =  Pundit.policy! users(:one), questions(:one)
+    assert_not policy.vote?
   end
+
+  test "can vote on some one else's question" do
+    policy =  Pundit.policy! users(:two), questions(:one)
+    assert policy.vote?
+  end
+
+  test "user can update their own question" do
+    policy =  Pundit.policy! users(:one), questions(:one)
+    assert policy.update?
+  end
+
+  test "cannot update some one else's question" do
+    policy =  Pundit.policy! users(:two), questions(:one)
+    assert_not policy.update?
+  end
+
+
+
 end
