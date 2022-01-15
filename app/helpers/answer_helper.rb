80840b6e3f6c29e13ccc2d41e134b14b1c8fb46a
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-15 09:41:28 +0000

    added answers_count to questions

--- app/helpers/answer_helper.rb
+++ app/helpers/answer_helper.rb
@@ -1,5 +1,5 @@
 module AnswerHelper
-  def answers_count(answers)
-    content_tag :span, pluralize(answers.count, 'Answer'), id: "answersCount"
+  def answers_count(count)
+    content_tag :span, pluralize(count, 'Answer'), id: "answersCount"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_095031) do
+ActiveRecord::Schema.define(version: 2022_01_15_090835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2022_01_12_095031) do
     t.bigint "subject_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "answers_count"
     t.index ["subject_id"], name: "index_questions_on_subject_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
