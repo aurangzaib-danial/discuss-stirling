143471786298d4b298d3e8ebf8c922e93eeb0e6a
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-15 20:39:46 +0000

    updated fixtures

--- test/policies/question_policy_test.rb
+++ test/policies/question_policy_test.rb
@@ -1,18 +1,26 @@
 require 'test_helper'
 
 class QuestionPolicyTest < ActiveSupport::TestCase
-  def test_scope
+  # def test_scope
+  # end
+
+  # def test_show
+  # end
+
+  # def test_create
+  # end
+
+  # def test_update
+  # end
+
+  # def test_destroy
+  # end
+
+  setup do
+    @policy = Pundit.policy! users(:one), questions(:one)
   end
 
-  def test_show
-  end
-
-  def test_create
-  end
-
-  def test_update
-  end
-
-  def test_destroy
+  test "cannot vote question if the user themselves created the question" do
+    
   end
 end
