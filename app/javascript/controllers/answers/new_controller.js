c964d0cdc2b20b9c0b0444bd918c16dd13bd5824
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-21 11:12:36 +0000

    renamed question-form-controller to answers/new-controller

--- app/javascript/controllers/answers/new_controller.js
+++ app/javascript/controllers/answers/new_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="question-form"
+
 export default class extends Controller {
   static targets = ["body", "errorMessage"]
 
