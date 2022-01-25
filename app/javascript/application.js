c4d7a7c1e8af5bff6dad458d33011719b3f3c81f
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-01-25 18:18:05 +0000

    switch from importmaps to webpack

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,7 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
 import "trix"
 import "@rails/actiontext"
-import "bootstrap"
-import "@popperjs/core"
 import "@rails/request.js"
+import * as bootstrap from "bootstrap"
