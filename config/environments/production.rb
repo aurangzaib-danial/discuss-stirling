111cf683ebbe8d03e76d087d6eae0da6a05de72f
Aurangzaib Khan <aurangzaib.danial@gmail.com>
2022-02-02 20:26:54 +0000

    Add default url options for mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: "discuss-stirling.co.uk" }
+
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: Rails.application.credentials.dig(:mailgun, :api_key),
